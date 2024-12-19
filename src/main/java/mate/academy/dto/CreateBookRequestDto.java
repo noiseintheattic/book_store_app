76f086d0e2344c4d585591a54b36c24c0351cf2d
Bartlomiej Smreczak <b.smreczak@gmail.com>
2024-12-19 21:23:31 +0100

    Added: BookDto, CreateBookRequestDto, Mapstruct, Mappers, EntityNotFoundException, BookController with methods: getAll(), getBookById(), createBook(). Removed: CommandLineRunner bean.

---
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -0,0 +1,17 @@
+package mate.academy.dto;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+public class CreateBookRequestDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+
+}
--- src/main/java/mate/academy/service/BookServiceImpl.java
+++ src/main/java/mate/academy/service/BookServiceImpl.java
@@ -1,28 +1,38 @@
 package mate.academy.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.dto.BookDto;
+import mate.academy.dto.CreateBookRequestDto;
+import mate.academy.mapper.BookMapper;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
 
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto add(CreateBookRequestDto createBookRequestDto) {
+        Book book = bookMapper.toModel(createBookRequestDto);
+        Book savedBook = bookRepository.add(book);
+        return bookMapper.toDto(savedBook);
     }
 
     @Override
-    public Book add(Book book) {
-        return bookRepository.add(book);
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(b -> bookMapper.toDto(b))
+                .toList();
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto getById(Long id) {
+        Book bookById = bookRepository.getById(id);
+        return bookMapper.toDto(bookById);
     }
 }
